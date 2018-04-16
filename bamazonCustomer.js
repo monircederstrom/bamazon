9d312071db39890e765ec49b68169e420bffb277
Monir Cederstrom <32554823+monircederstrom@users.noreply.github.com>
2018-04-16 18:27:06 -0500

    add if else check statement

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   queryAllProducts();
@@ -20,46 +20,49 @@ connection.connect(function(err) {
 });
 
 function queryAllProducts() {
-  connection.query("SELECT * FROM products", function(err, res) {
+  connection.query("SELECT * FROM products", function (err, res) {
     for (var i = 0; i < res.length; i++) {
       console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + "$" + res[i].price + " | " + res[i].stock_quantity);
     }
     console.log("-----------------------------------");
     userInput();
   });
-var userInput = function() {
-inquirer.prompt([
+  var userInput = function () {
+    inquirer.prompt([
 
-    {
-      type: "input",
-      name: "item_number",
-      message: "What is the number of the product you wish to purchase?"
-    },
-    {
+      {
+        type: "input",
+        name: "item_number",
+        message: "What is the number of the product you wish to purchase?"
+      },
+      {
         type: "input",
         name: "quantity",
         message: "How many do you wish to purchase?"
-    }
+      }
 
-]).then(function(answers){
-   var item_number = answers.item_number;
-   var quantity = answers.quantity;
-    console.log(item_number);
-    console.log(quantity);
-    // check to see if units purchased is < quantity, 
-    connection.query("SELECT * FROM products", function(err, res) {
+    ]).then(function (answers) {
 
-       var stock = res[i].stock_quantity;
-       console.log("stock is " + stock);
-    
-    })
+      // check to see if units purchased is < quantity,     
+      var query = connection.query("SELECT * FROM products WHERE item_id=?", answers.item_number, function (err, res) {
         // if yes then update SQL to subtract units purchased
-
+        if (answers.quantity > res[0].stock_quantity) {
+          console.log("Order too large. We can not fulfill your order.")
+        }
         // if no then console.log("Insufficient quantity!");
-        
-}).catch(function(err){
-    console.log(err);
-    });
-};
-connection.end();
+        else {
+          var query = connection.query("UPDATE products SET ? WHERE ?", [{
+              stock_quantity: res[0].stock_quantity - answers.quantity
+            },
+            {
+              item_id: answers.item_number
+            }
+          ]);
+          console.log("Your total cost is $" + answers.quantity * res[0].price.toFixed(2));
+        };
+        connection.end();
+
+      })
+    })
+  }
 }
